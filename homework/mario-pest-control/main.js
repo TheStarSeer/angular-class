8fa39ec962befe55481ec92a00bb89c7e153f1d1
Phillip Adamson <adamson.phillip2@gmail.com>
2015-10-04 17:37:20 +0000

    This adds the Mario Pest Control homework.

---
+++ homework/mario-pest-control/main.js
@@ -0,0 +1,4 @@
+document.getElementById("price1").innerHTML = (12 * 5) + " Coins";
+document.getElementById("price2").innerHTML = (8 * 7) + " Coins";
+document.getElementById("price3").innerHTML = (5 * 11) + " Coins";
+document.getElementById("total-price").innerHTML = (60 + 56 + 55) + " Coins";
